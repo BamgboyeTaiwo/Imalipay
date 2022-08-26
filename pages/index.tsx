e374064037f61293d001b5a94148472c295feaec
Bamgboye Taiwo <102883093+BamgboyeTaiwo@users.noreply.github.com>
2022-08-26 14:53:28 +0100

    edits to index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,4 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import Navbar from '../src/component/Navbar'
-import Sidebar from '../src/components/Sidebar/Sidebar'
 import Overview from '../src/UI/Overview/Overview'
 // import styles from '../styles/Home.module.css'
 
