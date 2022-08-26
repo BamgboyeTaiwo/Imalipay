a66440d12ab25303fea318342f5a4b97434c7478
Bamgboye Taiwo <102883093+BamgboyeTaiwo@users.noreply.github.com>
2022-08-26 14:58:52 +0100

    cards type error

--- src/components/Header/Headtitle.tsx
+++ src/components/Header/Headtitle.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function Headtitle({title}) {
+export default function Headtitle({title}:any) {
   return (
     <div className=' pt-[2.5%]'>
         <span className='text-lg text-[#222222] font-semibold font-inter'>{title}</span>
